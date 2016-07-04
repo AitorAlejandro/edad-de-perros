da7b0c3e7f88de8045b3c4f2f40ddb798b2d5be2
Aitor Alejandro Herrera <aitor.alejandro.herrera@gmail.com>
2016-07-04 14:57:53 +0200

    commit final

--- edad-de-perros.cpp
+++ edad-de-perros.cpp
@@ -1,2 +1,24 @@
 #include <iostream>
 #include <cmath>
+
+int edadPerro(int);
+
+int main()
+{
+  using namespace std;
+  
+  int anyosPerrunos;
+  cout << "Introduce la edad real de tu perro:" << endl;
+  cin >> anyosPerrunos;
+  
+  int anyosHumanizados = edadPerro(anyosPerrunos);
+  
+  cout << "La edad humana de tu perro es " << anyosHumanizados << endl;
+  system("pause");
+  return 0;
+}
+
+int edadPerro(int pEdad)
+{
+  return 7 * pEdad;
+}
